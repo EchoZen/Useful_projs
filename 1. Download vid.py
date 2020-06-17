a322eec8d06a03fa99fbbc48fbac2b4396f82190
EchoZen <zoe9955@gmail.com>
2020-06-17 10:44:11 +0800

    commit

---
+++ 1. Download vid.py
@@ -0,0 +1,13 @@
+import requests, os
+os.chdir(r'C:\Users\zchen\Documents\SBS y1\SBS Y1S2\BS1006 Principles of Genetics\Video')
+chunksize= 256
+
+url='src="https://www.youtube.com/d8072100-a6ae-42bb-9575-4bbfa74f6a26"></video>'
+url= url.replace('"','')
+url= url.split('src=')[1].split('></video')[0]
+
+r= requests.get(url, stream= True)
+with open('aot1.mp4','wb') as f:
+    for chunk in r.iter_content(chunk_size=chunksize):
+        f.write(chunk)
+
